29ca250a24ff3bcaef3d7fa93a683c9a36df8a7f
dickson <kr.d.tsl@gmail.com>
2018-02-24 16:07:52 -0500

    birth

---
+++ src/components/AddItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Add a post</h2>
+      </div>
+    )
+  }
+}
+
+export default AddItem;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import App from './App';
+import AddItem from './components/AddItem';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/add-item' component={AddItem} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);

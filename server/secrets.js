c7197887f9f40989600b907cdcda19525dfdac41
dickson <kr.d.tsl@gmail.com>
2018-07-30 19:15:33 -0400

    server worksssss

---
+++ server/secrets.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const secrets = {
+  dbUri: process.env.DB_URI,
+  local_api_port: process.env.API_PORT
+};
+
+export const getSecret = key => secrets[key];
--- server/server.js
+++ server/server.js
@@ -2,20 +2,53 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
+import { getSecret } from './secrets';
+import Comment from './models/comment';
 
+const API_PORT = getSecret('local_api_port') || 3001;
 const app = express();
 const router = express.Router();
 
-const API_PORT = process.env.API_PORT || 3001;
+mongoose.connect(getSecret('dbUri'), { useNewUrlParser: true })
+  .then(connection => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
 
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+router.get('/', (request, response) => {
+  response.json({ message: 'Hello, World!' });
+});
+
+router.get('/comments', (request, response) => {
+  Comment.find((error, comments) => {
+    if (error) return response.json({ success: false, error: error });
+    return response.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (request, response) => {
+  const comment = new Comment();
+  const { author, text } = request.body;
+  if (!author || !text) {
+    return response.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(error => {
+    if (error) return response.json({ success: false, error: error });
+    return response.json({ success: true });
+  });
 });
 
 app.use('/api', router);
 
-app.listen(API_PORT, () => console.log(`Listening on port $(API_PORT)`));
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
